3b30a7fae5eb69ebb81ae378bb514ca3cee10de8
Yurik Abdulayev <yurs11k89@gmail.com>
2016-08-26 22:52:12 +0300

    fix url

--- static/js/url.js
+++ static/js/url.js
@@ -3,8 +3,8 @@
  */
 function getUrl() {
     //    run in heroku
-    // return "https://team1-test-prof.herokuapp.com/";
+           return "https://team1-test-prof.herokuapp.com/";
     //    run in local host
-        return "http://127.0.0.1:5000/"
+    //     return "http://127.0.0.1:5000/"
 }
 
